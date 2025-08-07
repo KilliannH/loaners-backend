c69d94f4cf6e5fe4fb9c9d05db3d8543521d3d7c
killiannh <killiannhervagault@gmail.com>
2025-08-07 21:31:52 +0200

    prepare for deployment

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // ⚠️ à restreindre en prod
-    methods: ["GET", "POST"],
+    origin: "https://loaners.net", // ⚠️ à restreindre en prod
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   },
 });
 
