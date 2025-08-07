ffe8091d67d76cb5b7312791f81fe64b1d351c13
killiannh <killiannhervagault@gmail.com>
2025-08-08 00:22:51 +0200

    bugfix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const { getMe, getById, updateProfile } = require('../controllers/user.controller');
 const { authMiddleware } = require('../middleware/auth.middleware');
 
-console.log("✅ user.routes.js chargé");
-
 router.get('/me', authMiddleware, getMe);
 router.put('/me', authMiddleware, updateProfile);
 router.get('/:id', authMiddleware, getById);
