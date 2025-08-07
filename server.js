cc4816b35c8e212e4c4827f1ed59df7258bed162
killiannh <killiannhervagault@gmail.com>
2025-08-07 23:24:08 +0200

    debug

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://loaners.net", // ⚠️ à restreindre en prod
+    origin: "https://loners.net", // ⚠️ à restreindre en prod
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
